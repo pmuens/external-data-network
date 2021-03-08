e58cc0a88789148e4b277ffd969462efae26ae65
Philipp Muens <raute1337@gmx.de>
2021-03-08 20:08:52 +0100

    Setup Jest

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 const { log } = console
 
+export function sum(a: number, b: number): number {
+  return a + b
+}
+
 function main() {
-  log('Hello World!')
+  const result = sum(2, 4)
+  log(`2 + 4 = ${result}`)
 }
 
 main()
