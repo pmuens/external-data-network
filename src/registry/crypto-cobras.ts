d1d43ea68179e6875bf98393a4995de1cc2dbfe6
Philipp Muens <raute1337@gmx.de>
2021-03-22 07:57:12 +0100

    Update CryptoCobras to work with new GraphQL Input

--- src/registry/crypto-cobras.ts
+++ src/registry/crypto-cobras.ts
@@ -1,5 +1,3 @@
-import { GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
-
 import { DBOutput, DB, GraphQL, GraphQLInput } from '../host/types'
 import { Transformer } from '../host/interfaces'
 import { EthereumEvents, Output as EEOutput } from './ethereum-events'
@@ -16,45 +14,44 @@ export class CryptoCobras implements Transformer<DBOutput<EEOutput>, GraphQLInpu
   async transform(db: DB<EEOutput>, graphql: GraphQL): Promise<number> {
     let transformed = 0
 
-    const type = new GraphQLObjectType({
-      name: 'CryptoCobra',
-      fields: {
-        owner: { type: GraphQLString },
-        id: { type: GraphQLString },
-        rarity: { type: GraphQLInt },
-        genes: { type: GraphQLInt }
+    const types = `
+      type CryptoCobra {
+        owner: String
+        id: String
+        rarity: Int
+        genes: Int
       }
-    })
-    const args = {
-      id: { type: GraphQLString }
-    }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const resolve = async (_: unknown, args: any) => {
-      const klass = EthereumEvents
-      const filters = { address: this._address }
 
-      const cobras = await db.read({ klass, filters })
-      let result = cobras.map((item) => ({
-        owner: item.arguments[0],
-        id: item.arguments[1],
-        rarity: item.arguments[2],
-        genes: item.arguments[3]
-      }))
-      if (args.id) {
-        result = result.filter((item) => item.id === args.id)
+      type Query {
+        cryptoCobras(id: String): [CryptoCobra]
       }
-      return result
-    }
-    const fieldConfigs = [
-      {
-        name: 'cryptoCobras',
-        type: new GraphQLList(type),
-        args,
-        resolve
-      }
-    ]
+    `
 
-    graphql.write(CryptoCobras, fieldConfigs)
+    const resolvers = {
+      Query: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        cryptoCobras: async (_: unknown, args: any) => {
+          const klass = EthereumEvents
+          const filters = { address: this._address }
+
+          const cobras = await db.read({ klass, filters })
+          let result = cobras.map((item) => ({
+            owner: item.arguments[0],
+            id: item.arguments[1],
+            rarity: item.arguments[2],
+            genes: item.arguments[3]
+          }))
+          if (args.id) {
+            result = result.filter((item) => item.id === args.id)
+          }
+          return result
+        }
+      }
+    }
+
+    const data = [{ types, resolvers }]
+
+    graphql.write(CryptoCobras, data)
 
     transformed++
     return transformed
