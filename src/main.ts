a7fbd7d9ecb0704f546af8b359b72012bebd88bb
Philipp Muens <raute1337@gmx.de>
2021-03-23 15:40:37 +0100

    Update main file to use declarative Job configs

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 import { DB } from './db'
 import { Server } from './server'
 import { GraphQL } from './graphql'
+import { Job, Config as JobConfig } from './job'
 import { Scheduler } from './scheduler'
-import { EdnOracle, EthereumEvents, CryptoCobras, EthereumSmartContract } from './registry'
 
 dotenv.config()
 
@@ -16,61 +16,74 @@ declare const process: {
 
 const { CRYPTO_COBRAS_ADDRESS, ETHEREUM_RPC_URL, EDN_ORACLE_ADDRESS, SERVER_PORT } = process.env
 
-const { log } = console
+const configs: JobConfig[] = [
+  {
+    name: 'CobrasBirthEventIndexer',
+    logs: true,
+    run: {
+      type: 'schedule',
+      args: ['*/5 * * * * *']
+    },
+    source: {
+      name: 'EthereumEvents',
+      args: [ETHEREUM_RPC_URL, CRYPTO_COBRAS_ADDRESS, 'Birth(address,uint256,uint64,uint8)', 0]
+    },
+    sink: 'DB'
+  },
+  {
+    name: 'OracleDataRequestProcessor',
+    logs: true,
+    run: {
+      type: 'schedule',
+      args: ['*/5 * * * * *']
+    },
+    source: {
+      name: 'EthereumEvents',
+      args: [
+        ETHEREUM_RPC_URL,
+        EDN_ORACLE_ADDRESS,
+        'DataRequest(address,uint256,string,bytes,string,bytes)',
+        0
+      ]
+    },
+    sink: 'EthereumSmartContract',
+    transformer: 'EdnOracle'
+  },
+  {
+    name: 'CryptoCobrasTransformer',
+    logs: false,
+    run: 'startup',
+    source: 'DB',
+    sink: 'GraphQL',
+    transformer: {
+      name: 'CryptoCobras',
+      args: [CRYPTO_COBRAS_ADDRESS]
+    }
+  }
+]
 
 async function main() {
-  // --- Registry ---
-  // CryptoCobras
-  const cobras = new CryptoCobras(CRYPTO_COBRAS_ADDRESS)
-  // EdnOracle
-  const oracle = new EdnOracle()
-  // EthereumEvents
-  const cobraEvents = new EthereumEvents(
-    ETHEREUM_RPC_URL,
-    CRYPTO_COBRAS_ADDRESS,
-    'Birth(address,uint256,uint64,uint8)',
-    0
-  )
-  const oracleEvents = new EthereumEvents(
-    ETHEREUM_RPC_URL,
-    EDN_ORACLE_ADDRESS,
-    'DataRequest(address,uint256,string,bytes,string,bytes)',
-    0
-  )
-  // EthereumSmartContract
-  const oracleContract = new EthereumSmartContract()
-
-  // --- Core ---
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const db = new DB<any>()
   const server = new Server(parseInt(SERVER_PORT))
   const graphql = new GraphQL(server, db)
   const scheduler = new Scheduler()
+  const singletons = { db, graphql }
 
-  db.add(cobraEvents)
-  db.add(oracleEvents)
-  graphql.add(cobraEvents)
-  graphql.add(oracleEvents)
+  for (const config of configs) {
+    const job = new Job(config, singletons)
 
-  async function cobraEventWatcher() {
-    const result = await cobraEvents.read()
-    const processed = await db.write(result, cobraEvents)
-    log(`"cobraEventWatcher" --> Indexed ${processed} new entries...`)
+    const func = job.getFunc()
+    const { type, args } = job.getConfig()
+
+    if (type === 'startup') {
+      await func()
+    } else if (type === 'schedule') {
+      scheduler.add(func, args[0] as string)
+    }
   }
-
-  async function oracleEventWatcher() {
-    const processed = await oracle.transform(oracleEvents, oracleContract)
-    log(`"oracleEventWatcher" --> Processed ${processed} new requests...`)
-  }
-
-  await cobras.transform(db, graphql)
 
   graphql.setup()
-
-  scheduler.add(cobraEventWatcher, '*/5 * * * * *')
-  scheduler.add(oracleEventWatcher, '*/5 * * * * *')
-  await cobraEventWatcher()
-  await oracleEventWatcher()
 
   scheduler.start()
   server.start()
