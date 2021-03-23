fbb23fb0a8c32c1c658539a1820adce6e6c5e323
Philipp Muens <raute1337@gmx.de>
2021-03-23 15:36:15 +0100

    Implement first-pass of Job

---
+++ src/job.ts
@@ -0,0 +1,146 @@
+import { DB } from './db'
+import { GraphQL } from './graphql'
+import { loadModule } from './shared'
+import { Klass } from './types'
+import { Source, Sink, Transformer } from './interfaces'
+
+const { log } = console
+
+export class Job {
+  private _config: RunConfig
+  private _func: () => Promise<void>
+
+  constructor(config: Config, singletons: Singletons) {
+    const resolved = resolveJobConfig(config, singletons)
+    this._config = resolved.run
+    this._func = getFunction(resolved)
+  }
+
+  getConfig(): RunConfig {
+    return this._config
+  }
+
+  getFunc(): () => Promise<void> {
+    return this._func
+  }
+}
+
+export type Config = {
+  name: string
+  run: RunType | RunConfig
+  source: string | InterfaceConfig
+  sink: string | InterfaceConfig
+  transformer?: string | InterfaceConfig
+  logs?: boolean
+}
+
+function getFunction(config: ResolvedConfig): () => Promise<void> {
+  const { name, logs, source, sink, transformer } = config
+
+  if (!(source instanceof DB) && (sink instanceof DB || sink instanceof GraphQL)) {
+    sink.add(source)
+  }
+
+  return async function func() {
+    let processed = 0
+
+    if (!transformer) {
+      const data = await source.read()
+      if (sink instanceof DB) {
+        processed = await sink.write(data, source)
+      } else {
+        processed = await sink.write(data)
+      }
+    } else {
+      processed = await transformer.transform(source, sink)
+    }
+
+    if (logs && processed > 0) {
+      log(`"${name}": Processed "${processed}" item(s)...`)
+    }
+  }
+}
+
+function resolveJobConfig(config: Config, singletons: Singletons): ResolvedConfig {
+  const { name } = config
+  const logs = config.logs || false
+
+  const run = resolveRunConfig(config.run)
+
+  const source = getInstance(resolveInterfaceConfig(config.source), singletons)
+  const sink = getInstance(resolveInterfaceConfig(config.sink), singletons)
+  let transformer
+  if (config.transformer) {
+    transformer = getInstance(resolveInterfaceConfig(config.transformer), singletons)
+  }
+
+  return {
+    name,
+    logs,
+    run,
+    source,
+    sink,
+    transformer
+  }
+}
+
+function resolveInterfaceConfig(config: string | InterfaceConfig): InterfaceConfig {
+  let name
+  let args: unknown[] = []
+
+  if (typeof config === 'string') {
+    name = config
+  } else {
+    name = (config as InterfaceConfig).name
+    args = (config as InterfaceConfig).args
+  }
+
+  return { name, args }
+}
+
+function resolveRunConfig(config: RunType | RunConfig): RunConfig {
+  let type
+  let args: unknown[] = []
+
+  if (typeof config === 'string') {
+    type = config
+  } else {
+    type = (config as RunConfig).type
+    args = (config as RunConfig).args
+  }
+
+  return { type, args }
+}
+
+function getInstance(config: InterfaceConfig, singletons: Singletons) {
+  if (config.name === 'DB') return singletons.db
+  if (config.name === 'GraphQL') return singletons.graphql
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return loadModule(config.name, config.args) as any
+}
+
+type Singletons = {
+  db: Klass
+  graphql: Klass
+}
+
+type InterfaceConfig = {
+  name: string
+  args: unknown[]
+}
+
+type RunConfig = {
+  type: RunType
+  args: unknown[]
+}
+
+type RunType = 'schedule' | 'startup'
+
+type ResolvedConfig = {
+  name: string
+  logs: boolean
+  run: RunConfig
+  source: Source<unknown>
+  sink: Sink<unknown>
+  transformer?: Transformer<unknown, unknown>
+}
