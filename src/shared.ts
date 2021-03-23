2789ef51277046d5c1fd9ceb5638692ef080446d
Philipp Muens <raute1337@gmx.de>
2021-03-23 15:15:11 +0100

    Add function to load module from registry

--- src/shared.ts
+++ src/shared.ts
@@ -1,3 +1,9 @@
+import { join } from 'path'
+
+export function loadModule<T>(name: string, args: unknown[]): new () => T {
+  return loadFromRegistry(name, args)
+}
+
 export function toPascalCase(str: string): string {
   return str[0].toLocaleUpperCase() + str.slice(1, str.length)
 }
@@ -7,8 +13,29 @@ export function toCamelCase(str: string): string {
 }
 
 export function toSnakeCase(str: string): string {
+  return toSeparatedCase(str, '_')
+}
+
+export function toDashedCase(str: string): string {
+  return toSeparatedCase(str, '-')
+}
+
+function toSeparatedCase(str: string, sep: string): string {
   return (
     str[0].toLowerCase() +
-    str.slice(1, str.length).replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`)
+    str.slice(1, str.length).replace(/[A-Z]/g, (letter) => `${sep}${letter.toLowerCase()}`)
   )
+}
+
+function loadFromRegistry(name: string, args: unknown[]) {
+  const fileName = toDashedCase(name)
+  const registryPath = join(__dirname, 'registry')
+  const modulePath = join(registryPath, fileName)
+  return createInstance(modulePath, name, args)
+}
+
+function createInstance(path: string, name: string, args: unknown[]) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const Klass = require(path)[name]
+  return new Klass(...args)
 }
