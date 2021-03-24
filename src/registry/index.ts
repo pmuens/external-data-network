6d77901d65e1220b104740c5e2b9cb08b70935d8
Philipp Muens <raute1337@gmx.de>
2021-03-24 12:02:17 +0100

    Remove CryptoCobras from registry

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,7 +1,6 @@
-import { CryptoCobras } from './CryptoCobras'
 import { EdnOracle } from './EdnOracle'
 import { EthereumEvents } from './EthereumEvents'
 import { EthereumSmartContract } from './EthereumSmartContract'
 import { SimpleRng } from './SimpleRng'
 
-export { CryptoCobras, EdnOracle, EthereumSmartContract, EthereumEvents, SimpleRng }
+export { EdnOracle, EthereumSmartContract, EthereumEvents, SimpleRng }
