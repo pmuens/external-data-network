d0f649cd7f59e41de47d2f36ac589be1bf3509d1
Philipp Muens <raute1337@gmx.de>
2021-04-07 16:12:51 +0200

    Update variable names to camel case

--- src/main.ts
+++ src/main.ts
@@ -14,19 +14,19 @@ const { HOST, P2P_PORT, SERVER_PORT, P2P_BOOTSTRAP_MULTIADDR } = process.env
 
 async function main() {
   const host = HOST as string
-  const p2p_port = parseInt(P2P_PORT as string)
-  const server_port = parseInt(SERVER_PORT as string)
-  const p2p_bootstrap_multiaddr = P2P_BOOTSTRAP_MULTIADDR as string
+  const p2pPort = parseInt(P2P_PORT as string)
+  const serverPort = parseInt(SERVER_PORT as string)
+  const bootstrapMultiaddr = P2P_BOOTSTRAP_MULTIADDR as string
 
   const db = new DB()
-  const server = new Server(server_port)
+  const server = new Server(serverPort)
   const graphql = new GraphQL(server, db)
 
   const ctx: Context = { db, server, graphql }
 
   const scheduler = new Scheduler()
   const manager = new Manager(ctx, scheduler)
-  const networking = new Networking(ctx, host, p2p_port, p2p_bootstrap_multiaddr)
+  const networking = new Networking(ctx, host, p2pPort, bootstrapMultiaddr)
 
   await networking.setup()
   await manager.setup()
