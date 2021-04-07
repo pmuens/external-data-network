a47b6ec46ac1292a38f2adcb62222a64a5d9890f
Philipp Muens <raute1337@gmx.de>
2021-04-07 16:50:45 +0200

    Exclude dist directory from tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/dist/']
 }
